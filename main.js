ec44c68e5f91def272c6be07eb4d21dbc08b4a68
BGER1 <118640076+BGER1@users.noreply.github.com>
2026-02-26 01:22:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-<script type="importmap">
-{
-  "imports": {
-    "three": "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js"
-  }
-}
-</script>
 import { Viewer } from "./viewer.js";
 
 const viewer = Viewer();
